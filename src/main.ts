fdfdeb217681de6ef5bc0f343c57995baab9b4b9
YahiaMXRT <imnotapotato.632@gmail.com>
2026-07-05 09:17:27 +0000

    s

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
-ModAPI.displayToChat("hello, this is eaglerforge!")
+const m = ModAPI;
 
+m.addEventListener("sendchatmessage", (e: any) => {
+    if (e.message[0] == "!") {
+        e.preventDefault = true
+        if (e.message == "!fb on") {
+            m.settings.gammaSetting = 1000.0
+        } else if (e.message == "!fb off") {
+            m.settings.gammaSetting = 1.0
+        }
+    }
+})
